cf1eebef3699261950ef31d22daf49fdf3a02122
davicorreiajr <davicorreiajr@gmail.com>
2019-01-09 19:38:33 +0200

    Remove body-parser from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import errorHandler from 'errorhandler';
 import 'reflect-metadata';
 import { useExpressServer } from 'routing-controllers';
@@ -10,8 +9,6 @@ dotenv.config();
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') app.use(errorHandler());
 
 useExpressServer(app, {
