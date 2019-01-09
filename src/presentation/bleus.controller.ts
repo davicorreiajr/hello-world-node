2e70dfab95dcdf14e76ff37a069d46ffc1b792e7
davicorreiajr <davicorreiajr@gmail.com>
2019-01-09 19:38:38 +0200

    Improve bleus ctrl

--- src/presentation/bleus.controller.ts
+++ src/presentation/bleus.controller.ts
@@ -1,31 +1,30 @@
-import { Controller, Param, Body, Get, Post, Put, Delete } from 'routing-controllers';
+import { JsonController, Param, Body, Get, Post, Put, Delete } from 'routing-controllers';
 
-@Controller()
+@JsonController('/bleus')
 export class BleusController {
 
-    @Get('/bleus')
-    getAll() {
-       return 'This action returns all bleus';
-    }
+  @Get()
+  getAll() {
+    return 'This action returns all bleus';
+  }
 
-    @Get('/bleus/:id')
-    getOne(@Param('id') id: number) {
-       return 'This action returns bleus #' + id;
-    }
+  @Get('/:id')
+  getOne(@Param('id') id: number) {
+    return 'This action returns bleus #' + id;
+  }
 
-    @Post('/bleus')
-    post(@Body() bleus: any) {
-       return 'Saving bleus...';
-    }
+  @Post()
+  post(@Body() bleus: any) {
+    return 'Saving bleus...';
+  }
 
-    @Put('/bleus/:id')
-    put(@Param('id') id: number, @Body() bleus: any) {
-       return 'Updating a bleus...';
-    }
+  @Put('/:id')
+  put(@Param('id') id: number, @Body() bleus: any) {
+    return 'Updating a bleus...';
+  }
 
-    @Delete('/bleus/:id')
-    remove(@Param('id') id: number) {
-       return 'Removing bleus...';
-    }
-
+  @Delete('/:id')
+  remove(@Param('id') id: number) {
+    return 'Removing bleus...';
+  }
 }
