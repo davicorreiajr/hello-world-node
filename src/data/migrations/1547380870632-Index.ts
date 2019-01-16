e6e6a7d46a0ff5184d0a7d18eb40e54890cbbb60
davicorreiajr <davicorreiajr@gmail.com>
2019-01-16 17:57:24 +0200

    Fix files

--- src/data/migrations/1547380870632-Index.ts
+++ src/data/migrations/1547380870632-Index.ts
@@ -1,12 +1,12 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Index1547380870632 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+    async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`CREATE INDEX "IDX_ef0547411f8fcc42a4611570de" ON "bleus" ("email") `);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
+    async down(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`DROP INDEX "IDX_ef0547411f8fcc42a4611570de"`);
     }
 
