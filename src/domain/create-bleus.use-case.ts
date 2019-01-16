c9a62293c4b2fcbc3fd442566c01b2f275071210
davicorreiajr <davicorreiajr@gmail.com>
2019-01-16 18:06:49 +0200

    bleus

--- src/domain/create-bleus.use-case.ts
+++ src/domain/create-bleus.use-case.ts
@@ -1,12 +1,9 @@
-import { Service } from 'typedi';
-import { BleusRepository } from '../data';
+import { Service } from "typedi";
+import { BleusRepository } from "../data";
 
 @Service()
 export class CreateBleusUseCase {
-
-  constructor(
-    private repository: BleusRepository
-  ) {}
+  constructor(private repository: BleusRepository) {}
 
   execute(name: string, email: string) {
     return this.repository.create(name, email);
