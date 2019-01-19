0aa6697239ec16fd0090117a86796b9623cc96ad
davicorreiajr <davicorreiajr@gmail.com>
2019-01-19 20:57:42 +0200

    Finish bleus-get-all tests

--- test/requests/bleus-get-all.spec.ts
+++ test/requests/bleus-get-all.spec.ts
@@ -2,16 +2,46 @@ import request from 'supertest';
 import app from '../../src/app';
 import setUpDatabase from '../helpers/db-config';
 import { BleusController } from '../../src/presentation';
+import { getRepository } from 'typeorm';
 
-describe('Bleus get all', () => {
+describe('Bleus get all', async () => {
   setUpDatabase(app, BleusController);
+  const bleusToCreate = [
+    {
+      email: 'bleus1@bleus.com',
+      name: 'Bleus1'
+    },
+    {
+      email: 'bleus2@bleus.com',
+      name: 'Bleus2'
+    }
+  ];
+  beforeAll(async () => await await getRepository('Bleus').save(bleusToCreate));
 
-  it('bleus', done => {
+  it('returns JSON with code 200', done => {
     request(app)
       .get('/bleus')
-      .set('Content-Type', 'application/json')
       .then(response => {
         expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        done();
+      });
+  });
+
+  it('returns an array', done => {
+    request(app)
+      .get('/bleus')
+      .then(response => {
+        expect(response.body).toBeInstanceOf(Array);
+        done();
+      });
+  });
+
+  it('returns all bleus', done => {
+    request(app)
+      .get('/bleus')
+      .then(response => {
+        expect(response.body).toEqual(bleusToCreate);
         done();
       });
   });
