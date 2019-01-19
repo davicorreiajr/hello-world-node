6f7a9b7b4da5c8cf9a005d1656771fbdf2cec127
davicorreiajr <davicorreiajr@gmail.com>
2019-01-19 21:09:12 +0200

    Bleus creation tests

--- test/requests/bleus-creation.spec.ts
+++ test/requests/bleus-creation.spec.ts
@@ -5,17 +5,29 @@ import { BleusController } from '../../src/presentation';
 
 describe('Bleus creation', () => {
   setUpDatabase(app, BleusController);
+  const bleusToCreate = {
+    email: 'bleus1@bleus.com',
+    name: 'Bleus1'
+  };
 
-  it('bleus', done => {
+  it('returns JSON with code 200', done => {
     request(app)
       .post('/bleus')
-      .send({
-        email: 'davi2@gmail.com',
-        name: 'Davi2'
-      })
-      .set('Content-Type', 'application/json')
+      .send(bleusToCreate)
       .then(response => {
         expect(response.status).toBe(200);
+        expect(response.type).toBe('application/json');
+        done();
+      });
+  });
+
+  it('returns an object with the params sent', done => {
+    request(app)
+      .post('/bleus')
+      .send(bleusToCreate)
+      .then(response => {
+        expect(response.body.email).toBe(bleusToCreate.email);
+        expect(response.body.name).toBe(bleusToCreate.name);
         done();
       });
   });
